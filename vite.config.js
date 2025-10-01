003c2cdd6e06353f9c9cab5bc3a32a8b282e918c
Ahmad Ishfaq <ahmadishfaqdogar10@gmail.com>
2025-10-01 12:53:03 +0500

    updated content

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/the-hidden-pain/',
   plugins: [react()],
-   base: '/the-hidden-pain/',
+   
 })
